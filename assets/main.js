4e0b1d434625ce54c515eca2a15d9e873a5466bf
Emily Rendleman <emilyrendleman@gmail.com>
2021-06-03 15:27:45 -0500

    removed marker clusters, added spiderfied clusters

--- assets/main.js
+++ assets/main.js
@@ -14,7 +14,9 @@ const getLocCodes = () => {
         return response.json();
     })
     .then(data => {
-        let code = data[15].code;
+      // need to add a dropdown option in HTML for the IL county, then search the data for a match and call getBirds with that location code
+      // hard coded w/ cook county for now
+        let code = data[5].code;
         getBirds(code);
       })
     .catch(error => {
@@ -33,36 +35,61 @@ const getBirds = (locCode) => {
     redirect: 'follow'
   };
 
-  fetch(`https://api.ebird.org/v2/data/obs/${locCode}/recent?maxResults=5`, requestOptions)
+  fetch(`https://api.ebird.org/v2/data/obs/${locCode}/recent?maxResults=10`, requestOptions)
     .then(response => {
         return response.json();
     })
     .then(data => {
-      let lat = data[0].lat;
-      let lng = data[0].lng;
+      // empty arrays for storing the desired info from ebird
+      let locations = [];
 
-      initMap(lat, lng);
+      for (i = 0; i < data.length; i++) {
+        let dataLat = data[i].lat;
+        let dataLng = data[i].lng
+        let commonName = data[i].comName;
+        let sciName = data[i].sciName;
 
+        let locObj = { 
+          lat: dataLat, 
+          lng: dataLng, 
+          // can change this to be HTML formatted per the google maps api example
+          info: `Spotted at this location: ${commonName} (${sciName}).`
+        };
+
+        locations.push(locObj);
+      }
+
+      // calling the map generation function with all the info gathered from ebird
+      initMap(locations);
       })
     .catch(error => {
         console.error(error);
     });
 }
 
-// Initialize and add the map
-function initMap(lat, lng) {
-  // The location of bird sighting
-  const bird = { lat: lat, lng: lng };
-  // The map, centered at bird sighting
+function initMap(locations) {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 8,
-    center: bird,
+    zoom: 10,
+    center: locations[0],
   });
-  // The marker, positioned at bird sighting
-  const marker = new google.maps.Marker({
-    position: bird,
-    map: map,
+  const iw = new google.maps.InfoWindow();
+  const oms = new OverlappingMarkerSpiderfier(map, {
+    markersWontMove: true,
+    markersWontHide: true,
+    basicFormatEvents: true
   });
+
+  for (i = 0; i < locations.length; i ++) {
+    (function() {  // make a closure over the marker and marker data
+      var markerData = locations[i];  // e.g. { lat: 50.123, lng: 0.123, text: 'XYZ' }
+      var marker = new google.maps.Marker({ position: markerData });  // markerData works here as a LatLngLiteral
+      google.maps.event.addListener(marker, 'spider_click', function(e) {  // 'spider_click', not plain 'click'
+        iw.setContent(markerData.info);
+        iw.open(map, marker);
+      });
+      oms.addMarker(marker);  // adds the marker to the spiderfier _and_ the map
+    })();
+  }
 }
 
 getLocCodes();
